406b44eaeda855f1b19bf0a3a0bef8a03ee2c005
overallwarf47 <eduardolonderovieira@gmail.com>
2015-04-22 17:55:58 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -100,6 +100,6 @@
     }));
 
     //Inicia o bot e o extende quando for completamente carregado.
-    $.getScript("https://raw.githubusercontent.com/overallwarf47/EhAquiBOT/master/EhAquiBOT.js", extend);
+    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
 
 }).call(this);
