dcd8cccf46a361892bfaeee49a5ca5de6f6b484e
overallwarf47 <eduardolonderovieira@gmail.com>
2015-04-22 20:30:00 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -34,6 +34,62 @@
 
          */
 
+	bot.commands.cookieCommand = {
+                command: 'cookie',
+                rank: 'user',
+                type: 'startsWith',
+                cookies: ['te deu um biscoito de chocolate!',
+                    'te deu um biscoito macio, feito em casa, de aveia!',
+                    'te deu um biscoito liso, seco e velho. O último da caixa. Nojento.',
+                    'te dá um biscoito de açúcar. O que? Sem flocos? Se eu fosse você não aceitava...',
+                    'te dá um biscoito de chocolate. Não pera, isso são passas. Urgh!',
+                    'te dá um biscoito enorme. Cutucando esse biscoito ele te dá mais biscoitos. Estranho.',
+                    'te dá um biscoito da sorte. Ele diz: "Por que você não está trabalhando em algum projeto?"',
+                    'te dá um biscoito da sorte. Ele diz: "Siga os bons e aprenda com eles."',
+                    'te dá um biscoito da sorte. Ele diz: "Corra o risco!"',
+                    'te dá um biscoito da sorte. Ele diz: "Vai pra fora."',
+                    'te dá um biscoito da sorte. Ele diz: "Não esqueça os vegetais..."',
+                    'te dá um biscoito da sorte. Ele diz: "A vida coloca em nossa frente opções."',
+                    'te dá um biscoito da sorte. Ele diz: "Acredite em milagres, mas não dependa deles."',
+                    'te dá um biscoito da sorte. Ele diz: "Se se virar um pouco, vai ver as moças bonitas."',
+                    'te dá um biscoito da sorte. Ele diz: "Eu te amo."',
+                    'te dá um biscoito de ouro. Você não pode comer, pois é feito de ouro. Droga.',
+                    'te dá um biscoito Oreo com um copo de leite.',
+                    'te dá um biscoito de arco-íris feito com amor <3',
+                    'te dá um biscoito velho, que ficou na chuva. É mofado.',
+                    'te faz biscoitos frescos! Eles cheiram ótimo!'
+                ],
+                getCookie: function () {
+                    var c = Math.floor(Math.random() * this.cookies.length);
+                    return this.cookies[c];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatcookie);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nousercookie, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfcookie, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.cookie, {nameto: user.username, namefrom: chat.un, cookie: this.getCookie()}));
+                            }
+                        }
+                    }
+                }
+            },
         //Carregar o pacote do chat novamente para registrar todas as mudanças
         bot.loadChat();
 
@@ -42,6 +98,8 @@
     //Mudar as configurações padrões do bot e ter certeza que elas foram carregadas corretamente
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
+    	version: "1.0.0",
+        status: false,
         botName: "EhAqui BOT",
         language: "portuguese",
         startupCap: 1, // 1-200
